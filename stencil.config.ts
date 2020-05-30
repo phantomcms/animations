8dc03d205135956d4645b6ac237d4b70ba43349b
Nick Winner <design@winnerdigital.net>
2020-05-30 06:43:11 -0400

    :wrench: Configure environment to skip compiling AppRoot component in production

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,10 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'shift',
   taskQueue: 'async',
+  tsconfig:
+    process.env.NODE_ENV === 'production'
+      ? 'tsconfig-prod.json'
+      : 'tsconfig.json',
   outputTargets: [
     {
       type: 'dist',
